467b3cde8d001f06011f23efa006f24a622bd9e2
fadhoo <fadipetomi00@gmail.com>
2025-11-18 00:39:59 +0100

    init

--- src/routes/v1/partner.route.js
+++ src/routes/v1/partner.route.js
@@ -4,7 +4,7 @@ const { auth, apiKeyAuth } = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const partnerValidation = require('../../validations/partner.validation');
 const partnerController = require('../../controllers/partner.controller');
-const partnerBetsController = require('../../controllers/partner.bets.controller');
+// const partnerBetsController = require('../../controllers/partner.bets.controller');
 const partnerAuthController = require('../../controllers/partner.auth.controller');
 
 const router = express.Router();
